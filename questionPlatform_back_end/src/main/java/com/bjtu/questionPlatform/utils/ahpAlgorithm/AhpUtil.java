9766260c6a1c7ecb00af9c931665081690890a9f
TZY <1137721268@qq.com>
2021-04-18 22:23:47 +0800

    ahp正在写

---
+++ questionPlatform_back_end/src/main/java/com/bjtu/questionPlatform/utils/ahpAlgorithm/AhpUtil.java
@@ -0,0 +1,14 @@
+package com.bjtu.questionPlatform.utils.ahpAlgorithm;
+
+
+/**
+ * @program: questionPlatform_back_end
+ * @description: ahp util
+ * @author: 童振宇
+ * @create: 2021-04-18 14:05
+ * @version: 1.0
+ **/
+public class AhpUtil {
+    private int length;
+
+}
