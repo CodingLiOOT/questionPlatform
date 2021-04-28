a29b2e05fa795544dc386d6b8d227db9c62ba5e6
jjk9791 <m18790316665@163.com>
2021-04-28 23:11:58 +0800

    feat:查看列表接口

--- front_end/src/request/api.js
+++ front_end/src/request/api.js
@@ -55,35 +55,34 @@ export default class api {
   static p_Invitation=p=>post('/user/invitation',p);
 
   /**
-   * 指标列表接口，获取所有指标
+   * 指标类列表接口，获取所有指标类
    * @returns {Promise<unknown>}
    */
-  static g_getJudgementList=p=>get('file/getJudgementList',p);
+  static g_getJClassList=p=>get('judgement/getJClassList',p);
 
   /**
-   * 添加指标接口，将前端的新指标数据发送到后端
+   * 查看某一指标类接口，查看一个指标类
    * @param {Object} p [发送的参数]
    * @returns {Promise<unknown>}
    */
-  static p_newJudgement=p=>post('file/newJudgement',p);
+  static p_getOneJudgement=p=>post('judgement/getOneJudgement',p);
 
+  /**
+   * 添加指标类接口，将前端的新指标类数据发送到后端
+   * @param {Object} p [发送的参数]
+   * @returns {Promise<unknown>}
+   */
+  static p_newJudgement=p=>post('judgement/newJudgement',p);
+
+  /**
+   * 获取所有报告列表接口，返回报告列表
+   * @returns {Promise<unknown>}
+   */
+  static p_getAllReportList= ()=>post('judgement/getAllReportList',null);
   /**
    * 分配指标接口，发送报告id和指标id，返回刷新后的报告列表
    * @param {Object} p [发送的参数]
    * @returns {Promise<unknown>}
    */
-  static p_allocateJudgement=p=>post('file/p_allocateJudgement',p);
-
-  /**
-   * 专家列表接口，获取专家列表
-   * @returns {Promise<unknown>}
-   */
-  static g_getExpertList=p=>post('file/getExpertList',p);
-
-  /**
-   * 专家分配接口，发送报告关键词，获取分配专家
-   * @param {Object} p [发送的参数：报告编号、关键词、名称、创建日期]
-   * @returns {Promise<unknown>}
-   */
-  static p_allocateExpert=p=>post('file/allocateExpert',p);
+  static p_allocateJudgement=p=>post('judgement/allocateJudgement',p);
 }
