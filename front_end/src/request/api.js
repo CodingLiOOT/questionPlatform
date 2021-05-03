5a8b8f239a154c6030d8f9c0a68781e60a453413
rise0608 <562392631@qq.com>
2021-05-03 17:26:02 +0800

    feat:分配指标界面和接口初稿

--- front_end/src/request/api.js
+++ front_end/src/request/api.js
@@ -66,4 +66,11 @@ export default class api {
    * @returns {Promise<unknown>}
    */
   static p_getOneJudgement=p=>post('judgement/getOneJudgement',p);
+
+  /**
+   * 分配指标接口，发送报告id和指标id，返回刷新后的报告列表
+   * @param {Object} p [发送的参数]
+   * @returns {Promise<unknown>}
+   */
+  static p_allocateJudgement=p=>post('file/p_allocateJudgement',p);
 }
