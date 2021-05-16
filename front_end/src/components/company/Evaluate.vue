98e1d64602c99a9ad9bc360d6621d77d9d92bbf3
rise0608 <562392631@qq.com>
2021-05-16 22:11:14 +0800

    fix:调整了报告列表接口以及api的bug

--- front_end/src/components/company/Evaluate.vue
+++ front_end/src/components/company/Evaluate.vue
@@ -131,6 +131,38 @@ export default {
       }
       return false;
     },
+    getAllReportList() {
+      this.$API.p_getAllReportList()
+        .then(
+          data => {
+            for (let i = 0; i < data.reports.length; i++) {
+              let temp = {
+                id: '',
+                name: '',
+                time: '',
+                tag: [],
+                information: '',
+              };
+              temp.id = data.reports[i].reportId;
+              temp.name = data.reports[i].reportName;
+              temp.time = data.reports[i].createTime;
+              temp.information = data.reports[i].JClassName;
+              if (temp.information === '') {
+                temp.information = '未分配';
+              }
+              for (let j = 0; j < data.reports[i].keyWord.length; j++) {
+                let k = data.reports[i].keyWord[j].word;
+                temp.tag.push(k);
+              }
+              this.tableData.push(temp);
+            }
+          }
+        )
+        .catch(
+          error => {
+          }
+        )
+    },
     getFilters() {
       for (let item in this.tableData) {
         for (let tag in this.tableData[item].tag) {
@@ -155,44 +187,9 @@ export default {
       this.filters = array;
     },
   },
-  getList(){
-    this.$API.p_getAllReportList({
-    })
-      .then(
-        data=>{
-          for(let i=0;i<data.reports.length;i++){
-            let temp={
-              id: '',
-              name: '',
-              time: '',
-              tag:[],
-              information: '',
-            };
-            temp.id=data.reports[i].reportId;
-            temp.name=data.reports[i].reportName;
-            temp.time=data.reports[i].createTime;
-            temp.information=data.reports[i].JClassName;
-            if(temp.information==='') {
-              temp.information='未分配';
-            }
-            for(let j=0;j<data.reports[i].keyWord.length;j++){
-              let k=data.reports[i].keyWord[j].word;
-              temp.tag.push(k);
-            }
-            // if(temp.id===data.reports[i].keyWords.reportId)
-            //   temp.tag=data.reports[i].keyWords.word;
-            this.tableData.push(temp);
-          }
-        }
-      )
-      .catch(
-        error=>{
-        }
-      )
-  },
   mounted() {
+    this.getAllReportList();
     this.getFilters();
-    this.getList();
   }
 }
 </script>
