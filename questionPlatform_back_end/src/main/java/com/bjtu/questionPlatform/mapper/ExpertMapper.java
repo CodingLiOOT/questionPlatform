b6a36716921ab3b73a3e1870363c656b2647e879
TZY <1137721268@qq.com>
2021-06-07 18:14:39 +0800

    fest: 专家登录邀请

--- questionPlatform_back_end/src/main/java/com/bjtu/questionPlatform/mapper/ExpertMapper.java
+++ questionPlatform_back_end/src/main/java/com/bjtu/questionPlatform/mapper/ExpertMapper.java
@@ -15,12 +15,16 @@ public interface ExpertMapper {
 
 
     @Select("select * from expert where expertName = #{expertName}")
-    Expert selectExpertByUserName(String expertName);
+    Expert selectExpertByExpertName(String expertName);
     @Select("select * from expert where keysId = #{keysId}")
     List<User> selectExpertByKeysId(String keysId);
+
+
+    @Select("select mail from expert where expertName = #{expertName}")
+    String selectMailByExpertName(String expertName);
     @Insert("insert into expert (expertName,keysId,expertType,mail,phone,expertUnit,expertInformation) "+
             "values (#{expertName},1,1,#{mail},#{phone},#{expertUnit},#{expertInformation})")
-    //插入的专家类型暂时没想好咋写
+        //插入的专家类型暂时没想好咋写
     void createExpert(Expert expert);
 
     @Select("select * from score where reportid = #{reportId} and judgementid = #{judgementId}")
