52a33397258204d40272d270bac893358c85420b
nancy_wdi <nancy_wdi@163.com>
2021-06-08 19:29:58 +0800

    feat:报告修改状态3，4

--- questionPlatform_back_end/src/main/java/com/bjtu/questionPlatform/mapper/ExpertMapper.java
+++ questionPlatform_back_end/src/main/java/com/bjtu/questionPlatform/mapper/ExpertMapper.java
@@ -35,4 +35,12 @@ public interface ExpertMapper {
         //插入的专家类型暂时没想好咋写
     void inviteExpert(String expertName,String reportId);
 
+    @Select("select * from keyword")
+    List<KeyWord> getAllKeys();
+
+    @Insert("insert into expertReport (expertName,reportId,finish) "+
+            "values (#{expertName},#{reportId},0")
+        //插入的专家类型暂时没想好咋写
+    void inviteExpert(String expertName,String reportId);
+
 }
