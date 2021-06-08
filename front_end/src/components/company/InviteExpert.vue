2cab60562db883d90d54d1f383ad587486fae07e
rise0608 <562392631@qq.com>
2021-06-08 23:09:45 +0800

    fix: 微调邀请专家界面

--- front_end/src/components/company/InviteExpert.vue
+++ front_end/src/components/company/InviteExpert.vue
@@ -7,50 +7,71 @@
     <br/>
 
     <el-form ref="form" :model="form" label-width="100px">
+      <el-form-item label="报告名称：">
+        <el-select v-model="form" filterable placeholder="请选择">
+          <el-option
+            v-for="item in reports"
+            :key="item"
+            :label="item.reportName"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+
+    <el-form ref="form" :model="form" label-width="100px">
       <el-form-item label="报告编号：">
-        <el-input v-model="form.reportId" placeholder=""></el-input>
+        <el-input v-model="form.reportId" placeholder="" :disabled="true"></el-input>
       </el-form-item>
       <el-form-item label="报告名称：">
-        <el-input v-model="form.reportName" placeholder=""></el-input>
+        <el-input v-model="form.reportName" placeholder="" :disabled="true"></el-input>
       </el-form-item>
       <el-form-item label="关键词：">
-        <el-input v-model="form.keyWords" placeholder="">
-          <el-select v-model="form.keyWords" slot="prepend" placeholder="请选择">
-            <el-option label="石油" value="石油"></el-option>
-            <el-option label="井" value="井"></el-option>
-            <el-option label="国家能源" value="国家能源"></el-option>
-            <el-option label="电" value="电"></el-option>
-            <el-option label="安全" value="安全"></el-option>
-            <el-option label="煤矿" value="煤矿"></el-option>
-          </el-select>
-        </el-input>
-
+        <el-input v-model="form.keyWords" placeholder="" :disabled="true"></el-input>
       </el-form-item>
 
       <el-form-item>
         <el-button type="primary" @click="invite">发送邀请码</el-button>
       </el-form-item>
     </el-form>
-
-
   </div>
-
 </template>
 
 <script>
 export default {
   name: "InviteExpert",
-  data(){
-    return{
-      form:{
-        reportId:'',
-        reportName:'',
-        keyWords:'',
-      }
-
+  data() {
+    return {
+      form: {
+        reportId: '',
+        reportName: '',
+        createTime: '',
+        keyWords: [],
+      },
+      reports: [
+      //   {
+      //   reportId: '1',
+      //   reportName: '报告1',
+      //   createTime: '2020-6-6',
+      //   keyWords: ["石油","中石油"],
+      //   jClassName: '未分配',
+      // },{
+      //   reportId: '2',
+      //   reportName: '报告2',
+      //   createTime: '2020-6-7',
+      //   keyWords: ["中石油","中石化"],
+      //   jClassName: '未分配',
+      // },{
+      //   reportId: '3',
+      //   reportName: '报告3',
+      //   createTime: '2020-6-9',
+      //   keyWords: ["关键词1"],
+      //   jClassName: '未分配',
+      // },
+      ],
     }
   },
-  methods:{
+  methods: {
     invite() {
       let expertName = this.UrlSearch();
       this.$API.p_inviteExpert({
@@ -69,7 +90,42 @@ export default {
     UrlSearch() {
       var name = this.$route.query.name;
       return name;
-    }
+    },
+    getAllReportList() {
+      this.$API.p_getAllReportList()
+        .then(
+          data => {
+            for (let i = 0; i < data.reports.length; i++) {
+              let temp = {
+                reportId: '',
+                reportName: '',
+                createTime: '',
+                keyWords: [],
+                jClassName: '',
+              };
+              temp.reportId = data.reports[i].reportId;
+              temp.reportName = data.reports[i].reportName;
+              temp.createTime = data.reports[i].createTime;
+              temp.jClassName = data.reports[i].jClassName;
+              if (temp.jClassName == null) {
+                temp.jClassName = '未分配';
+              }
+              for (let j = 0; j < data.reports[i].keyWord.length; j++) {
+                let k = data.reports[i].keyWord[j].word;
+                temp.keyWords.push(k);
+              }
+              this.reports.push(temp);
+            }
+          }
+        )
+        .catch(
+          error => {
+          }
+        )
+    },
+  },
+  mounted() {
+    this.getAllReportList();
   },
 }
 </script>
